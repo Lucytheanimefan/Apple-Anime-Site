9c41cba36b52a36dfd2ea3cd9c49b99ee2550bfb
Lucy Zhang <lucy_zhang@apple.com>
2017-06-17 11:39:17 -0700

    Added iphone

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,22 @@
 var height = $(window).height();
-var imacRatio = 0.7036; //screen height by full height of imac
-var imacScreenRatio = 1.63; //width by height
+var imacRatio = 0.68; //screen height by full height of imac
+var imacScreenRatio = 1.71; //width by height
 
 
 $(document).ready(function() {
-    var imacHeight = height * 0.9;
+    var imacHeight = height * 0.85;
     var iframeHeight = imacHeight * imacRatio;
     var iframeWidth = iframeHeight * imacScreenRatio;
-    console.log($("#imac").width() +"-" + iframeWidth);
-    console.log(iframePadding)
+    var imacScreenHeight = imacHeight * 0.75;
     $("#imac").css("height", imacHeight + "px");
-    var iframePadding = ($("#imac").width() - iframeWidth) / 2;
-    $("#youtube").css({ "width": iframeWidth, "height": iframeHeight, "padding-left": iframePadding, "padding-right": iframePadding });
+    var imacWidth = $("#imac").width();
+     $("body").css("min-width",imacWidth);
+    var iframePadding = (imacWidth - iframeWidth) / 2;
+    var iframePaddingTop = (imacScreenHeight - iframeHeight)/2;
+    $("#youtube").css({ "width": iframeWidth, "height": iframeHeight, "padding-left": iframePadding, "padding-right": iframePadding, "padding-top":iframePaddingTop });
+    $("section").css("min-height",imacHeight+"px");
 
-    
+
 
 
 })
