642db05e7edc3a5bc31d2e3bb52a2f4f3bfb1333
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-07-01 15:58:47 -0700

    Navbar

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ def mal_page(username=None):
 @app.route("/credentials", methods=["GET"])
 def get_od_credentials():
 	name = request.headers.get('X-Forwarded-User')
-	return str(name)#str(first_name) + " " + str(last_name)
+	return str(name)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=port)
