6ee5ae85bf804da79262ec365ecdd7fa32e9d8db
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-07-02 13:04:19 -0700

    deploy

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def get_od_credentials():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True)
-    #app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
