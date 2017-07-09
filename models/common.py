79519b7856e86cb437d00ade2f5d140215b5c849
Terrence Katzenbaer <tkatzenbaer@apple.com>
2017-07-08 23:11:14 -0700

    Adds MAL Models

--- models/common.py
+++ models/common.py
@@ -2,8 +2,12 @@ from commons import db
 
 
 class User(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
+
+    mal_user = db.relationship("MalUser", uselist=False, back_populates="user")
 
     def __init__(self, username):
         self.username = username
---
+++ models/mal.py
@@ -0,0 +1,53 @@
+from commons import db
+import enum
+
+from datetime import datetime
+
+
+class MalUser(db.Model):
+    __tablename__ = 'mal_user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User", back_populates="mal_user")
+
+    entries = db.relationship("MalEntry", back_populates="mal_user")
+
+    def __init__(self, user):
+        self.user = user
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+
+class MalEntryUserStatus(enum.Enum):
+    watching = 1
+    completed = 2
+    on_hold = 3
+    dropped = 4
+    plan_to_watch = 6
+
+
+class MalEntryAiringStatus(enum.Enum):
+    airing = 1
+    aired = 2
+    not_aired = 3
+
+
+class MalEntry(db.Model):
+    __tablename__ = 'mal_entry'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    mal_user_id = db.Column(db.Integer, db.ForeignKey('mal_user.id'))
+    mal_user = db.relationship("MalUser", back_populates="entries")
+
+    anime_id = db.Column(db.Integer)
+    title = db.Column(db.String(80))
+    user_status = db.Column(db.Enum(MalEntryUserStatus))
+    airing_status = db.Column(db.Enum(MalEntryAiringStatus))
+    watched_episodes = db.Column(db.Integer)
+    total_episodes = db.Column(db.Integer)
+    user_score = db.Column(db.Integer)
